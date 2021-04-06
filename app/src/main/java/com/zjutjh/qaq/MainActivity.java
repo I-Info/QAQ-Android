2fd114ecf3deea28eaffa7132826522203b6390f
I-Info <i-in-fo@qq.com>
2021-04-07 00:02:46 +0800

    update

--- app/src/main/java/com/zjutjh/qaq/MainActivity.java
+++ app/src/main/java/com/zjutjh/qaq/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,35 +17,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSubmit(View view) {
-        String serverIp = ((EditText) findViewById(R.id.serverIp)).getText().toString();
-        String portString = ((EditText) findViewById(R.id.serverPort)).getText().toString();
-        String userName = ((EditText) findViewById(R.id.userName)).getText().toString();
+        EditText textServerIp = (EditText) findViewById(R.id.serverIp);
+        EditText textServerPort = (EditText) findViewById(R.id.serverPort);
+        EditText textUsername = (EditText) findViewById(R.id.username);
+
+        String serverIp = textServerIp.getText().toString();
+        String portString = textServerPort.getText().toString();
+        String userName = textUsername.getText().toString();
 
         if (serverIp.equals("")) {
-            ((EditText) findViewById(R.id.serverIp)).setError("Please input server IP address");
+            textServerIp.setError("Please input server IP address");
             return;
         }
 
         if (portString.equals("")) {
-            ((EditText) findViewById(R.id.serverPort)).setError("Please input server port");
+            textServerPort.setError("Please input server port");
             return;
         }
 
         if (userName.equals("") || userName.length() > 10) {
-            ((EditText) findViewById(R.id.userName)).setError("Invalid username");
+            textUsername.setError("Invalid username");
         }
 
         int serverPort = Integer.parseInt(portString);
         if (serverPort < 0 || serverPort > 65535) {
-            ((EditText) findViewById(R.id.serverPort)).setError("Invalid server port");
+            textServerPort.setError("Invalid server port");
             return;
         }
 
         String regExp = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
         Pattern pattern = Pattern.compile(regExp);
         if (!pattern.matcher(serverIp).matches()) {
-            ((EditText) findViewById(R.id.serverIp)).setError("Invalid IP address");
-            return;
+            textServerIp.setError("Invalid IP address");
         }
 
 
