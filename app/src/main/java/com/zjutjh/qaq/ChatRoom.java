15d700918cc287ecc6cba388776fd6490e5cb163
I-Info <i-in-fo@qq.com>
2021-04-07 21:06:45 +0800

    update

--- app/src/main/java/com/zjutjh/qaq/ChatRoom.java
+++ app/src/main/java/com/zjutjh/qaq/ChatRoom.java
@@ -2,13 +2,24 @@ package com.zjutjh.qaq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ChatRoom extends AppCompatActivity {
+    String serverIp;
+    int serverPort;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
+
+        Intent intent = getIntent();
+        serverIp = intent.getStringExtra(MainActivity.EXTRA_SERVER_IP);
+        serverPort = intent.getIntExtra(MainActivity.EXTRA_SERVER_PORT, 8080);
+        username = intent.getStringExtra(MainActivity.EXTRA_USERNAME);
+
+
     }
 }
