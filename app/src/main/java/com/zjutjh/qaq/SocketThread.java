d9fcd17f2c03d474b5dfc01b948aa531528d7c4e
I-Info <i-in-fo@qq.com>
2021-04-08 13:11:44 +0800

    优化解包算法.

--- app/src/main/java/com/zjutjh/qaq/SocketThread.java
+++ app/src/main/java/com/zjutjh/qaq/SocketThread.java
@@ -4,6 +4,10 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
+import androidx.collection.ArraySet;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
 import java.io.OutputStream;
 import java.io.InputStreamReader;
@@ -39,58 +43,40 @@ public class SocketThread implements Runnable {
             new Thread() {
                 @Override
                 public void run() {
-                    char[] contentRaw = new char[1024];
+                    char[] contentChar = new char[1024];
                     String content;
-                    String temp = "";
-                    int startIndex, endIndex;
+                    StringBuilder message = new StringBuilder();
+                    boolean startFlag = false;
                     try {
-                        while (bufferedReader.read(contentRaw) != -1) {
-                            content = String.valueOf(contentRaw);
-                            while (!content.isEmpty()) {
-                                //对获取到的字符串进行循环处理
+                        while (bufferedReader.read(contentChar) != -1) {
+                            content = String.valueOf(contentChar);
+                            for (int index = 0; index < content.length(); index++) {
                                 try {
-                                    if (temp.isEmpty()) {
-                                        startIndex = content.indexOf('{');
-                                        if ((endIndex = content.indexOf('}')) != -1) {
-                                            String messageString = content.substring(startIndex + 1, endIndex);
-                                            content = content.substring(endIndex + 1);
-                                            //对获取的信息进行处理
-                                            messageHandler(messageString);
-
-                                        } else {
-                                            //不存在结束标志符，content内容存入temp
-                                            temp = content.substring(startIndex + 1);
-                                            break;
-                                        }
-                                    } else {
-                                        //存在之前未结束的数据流
-                                        startIndex = content.indexOf('{');
-                                        endIndex = content.indexOf('}');
-                                        if (startIndex == -1 && endIndex == -1) {
-                                            //头尾标志符均未出现，content仍然为之前封包的一部分
-                                            temp = temp.concat(content);
-                                            break;
-                                        } else if (endIndex != -1 && endIndex < startIndex) {
-                                            //出现了结束标志符，且结束标识符在开始标志符之前(或开始标识符不存在)（正常情况）
-                                            String messageString = temp.concat(content.substring(0, endIndex));
-                                            temp = "";//temp清空
-                                            content = content.substring(endIndex + 1);
-                                            //对获取的信息进行处理
-                                            messageHandler(messageString);
-
-
-                                        } else if (endIndex == -1) {
-                                            //不存在结束标志符，但存在开始标志符（异常情况），丢弃temp，抛出异常。
-                                            temp = "";
+                                    if (content.charAt(index) == '{') {
+                                        if (startFlag) {
+                                            //异常情况，丢弃之前的message，抛出错误
+                                            message = new StringBuilder();
                                             throw new Exception("message without '}' endpoint");
+                                        } else {
+                                            //message开始
+                                            startFlag = true;
                                         }
+                                    } else if (content.charAt(index) == '}' && startFlag) {
+                                        //message结束
+                                        startFlag = false;
+                                        messageHandler(message.toString());
+                                        message = new StringBuilder();
+
+                                    } else if (startFlag) {
+                                        //正常情况将字符添加到message
+                                        message.append(content.charAt(index));
                                     }
+
                                 } catch (Exception exception) {
-                                    //处理字符串处理过程中的异常
                                     System.out.println(exception.getMessage());
                                 }
-
                             }
+
                         }
                     } catch (
                             IOException exception) {
@@ -98,8 +84,10 @@ public class SocketThread implements Runnable {
                     }
                 }
 
-                private void messageHandler(String rawMessage) {
+                private void messageHandler(@NotNull String rawMessage) {
                     //处理已经解包后的接收到的消息
+                    String[] messageArray = rawMessage.split("&;");
+
 
                 }
             }.start();
