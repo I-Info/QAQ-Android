1c473e64294c7a04e44a82e71a4c99b88dfafb5c
I-Info <49438478+I-Info@users.noreply.github.com>
2021-04-23 20:14:34 +0800

    v1.1.3 build

--- app/src/main/java/com/zjutjh/qaq/About.java
+++ app/src/main/java/com/zjutjh/qaq/About.java
@@ -11,8 +11,5 @@ public class About extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        TextView textView = findViewById(R.id.github_release);
-        textView.setText(Html.fromHtml("<a href=\"https://github.com/I-Info/QAQ-Android/releases\"><img alt=\"GitHub release (latest by date including pre-releases)\" src=\"https://img.shields.io/github/v/release/I-Info/QAQ-Android?include_prereleases&style=flat-square\"></a>",Html.FROM_HTML_MODE_LEGACY));
     }
 }
