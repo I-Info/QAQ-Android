160bdaa9c6c602617fd56c37c989742745c3dd33
I-Info <49438478+I-Info@users.noreply.github.com>
2021-04-23 17:46:58 +0800

    building,bug fixed

--- app/src/main/java/com/zjutjh/qaq/About.java
+++ app/src/main/java/com/zjutjh/qaq/About.java
@@ -3,6 +3,8 @@ package com.zjutjh.qaq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        TextView textView = findViewById(R.id.github_release);
+        textView.setText(Html.fromHtml("<a href=\"https://github.com/I-Info/QAQ-Android/releases\"><img alt=\"GitHub release (latest by date including pre-releases)\" src=\"https://img.shields.io/github/v/release/I-Info/QAQ-Android?include_prereleases&style=flat-square\"></a>",Html.FROM_HTML_MODE_LEGACY));
     }
 }
